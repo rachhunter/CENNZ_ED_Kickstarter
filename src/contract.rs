b051e5352e4a0b14e4a618b370f84f9b5b517e25
rachhunter <29443501+rachhunter@users.noreply.github.com>
2019-05-26 15:17:25 +1200

    Attempt 1st deploy

--- src/contract.rs
+++ src/contract.rs
@@ -4,25 +4,36 @@
 /// The CENNZnet SDK
 use contract_sdk::{prelude::*, types::AccountId, util};
 use ink_lang::contract;
+use ink_core::storage;
 
 contract! {
-    /// A contract that transfers a random amount of asset to the given player account
-    struct Spin2Win {}
 
-    impl Spin2Win {
-        pub(external) fn spin(&self, player: AccountId) {
-            let prize = util::random_in_range(1_000, 100_000);
-            Runtime::call(
-                player,
-                0, // gas allocation, `0` means use current meter value
-                prize.into(),
-                &vec![], // Empty input payload
-            );
+    struct KickStarter {
+        /// Set up CREATOR_ACCOUNT_ADDRESS and PROJECT_ID
+        creator: storage::Value<AccountId>,
+        project: storage::Value<u32>,
+    }
+
+    // Initiates CREATOR_ACCOUNT_ADDRESS and PROJECT_ID
+    impl Deploy for KickStarter {
+        fn deploy(&mut self) {
+            self.creator.set(env.caller()),
+            self.project.set(0),
         }
     }
 
-    impl Deploy for Spin2Win {
-        fn deploy(&mut self) {}
-    }
+    impl KickStarter {
 
+        /// Returns the current state of CREATOR_ACCOUNT_ADDRESS.
+        pub(external) fn address(&self) -> AccountId {
+            //println(&format!("Creator Address: {:?}", *self.create));
+            *self.creator,
+        }
+
+        // Returns the current state of PROJECT_ID
+        pub(external) fn id(&self) -> u32 {
+            //println(&format!("Project ID: {:?}", *self.project));
+            *self.project,
+        }
+    }
 }

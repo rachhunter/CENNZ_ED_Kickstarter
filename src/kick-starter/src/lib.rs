e600742b57691c2f9d5a3e8dd8761d8a4333da55
rachhunter <29443501+rachhunter@users.noreply.github.com>
2019-05-26 16:11:42 +1200

    Final deployed and called contract

--- src/kick-starter/src/lib.rs
+++ src/kick-starter/src/lib.rs
@@ -1,15 +1,40 @@
+// Example Deployed smart contract to repurpose for Kickstarter project
 #![no_std]
+
+/// The CENNZnet SDK
+use contract_sdk::{prelude::*, types::AccountId, util};
 use ink_lang::contract;
+use ink_core::storage;
 
 contract! {
-  // Define contract data
-  struct Demo {}
 
-  // Define contract functions
-  impl Demo {}
+    struct KickStarter {
+        /// Set up CREATOR_ACCOUNT_ADDRESS and PROJECT_ID
+        creator: storage::Value<AccountId>,
+        project: storage::Value<u32>,
+    }
 
-  // Define contract instantiation logic
-  impl Deploy for Demo {
-    fn deploy(&mut self) {}
-  }
+    // Initiates CREATOR_ACCOUNT_ADDRESS and PROJECT_ID
+    impl Deploy for KickStarter {
+        fn deploy(&mut self) {
+            // self.creator.set(env.caller());
+            self.creator.set(Default::default());
+            self.project.set(0);
+        }
+    }
+
+    impl KickStarter {
+
+        /// Returns the current state of CREATOR_ACCOUNT_ADDRESS.
+        pub(external) fn address(&self) -> AccountId {
+            //println(&format!("Creator Address: {:?}", *self.create));
+            *self.creator
+        }
+
+        // Returns the current state of PROJECT_ID
+        pub(external) fn id(&self) -> u32 {
+            //println(&format!("Project ID: {:?}", *self.project));
+            *self.project
+        }
+    }
 }
